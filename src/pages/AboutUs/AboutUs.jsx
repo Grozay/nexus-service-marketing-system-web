e9cefc6e66c5b6a62d92860095cf992cfdb3dcc8
Quyendev <146425082+Grozay@users.noreply.github.com>
2025-02-18 13:54:27 +0700

    update scrollanimation for aboutus page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -20,6 +20,9 @@ import teamMember1 from '~/assets/team-member-1.png'
 import teamMember2 from '~/assets/team-member-2.png'
 import teamMember3 from '~/assets/team-member-3.png'
 import AppBar from '~/components/AppBar/AppBar'
+import ScrollAnimation from 'react-animate-on-scroll'
+
+
 const AboutUsPage = () => {
   const teamData = [
     { id: 1, name: 'John Smith', title: 'CEO & Founder', description: 'Visionary leader with strategic thinking...', avatarUrl: teamMember1 },
@@ -76,160 +79,168 @@ const AboutUsPage = () => {
             >
               <BusinessRoundedIcon color="primary" fontSize="medium"/>
             </Icon>
-                    Our Story
+            Our Story
           </Typography>
           <Typography variant="body1" color="text.secondary">
-                    [**Insert &quot;NEXUS Story&quot; content here.** Example: Introduction to the founding time, reasons for establishment, initial difficulties, development process, important milestones, achievements, vision for the future,...]
-                    Tell an engaging, authentic, and inspiring story about NEXUS&apos;s journey. Emphasize the initial mission and how NEXUS overcame challenges to achieve its current position.
+                      [**Insert &quot;NEXUS Story&quot; content here.** Example: Introduction to the founding time, reasons for establishment, initial difficulties, development process, important milestones, achievements, vision for the future,...]
+                      Tell an engaging, authentic, and inspiring story about NEXUS&apos;s journey. Emphasize the initial mission and how NEXUS overcame challenges to achieve its current position.
           </Typography>
         </Box>
         <Divider sx={{ mb: 4 }} />
 
-
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
-            <Icon
-              sx={{
-                mr: 1
-              }}
-            >
-              <LightbulbRoundedIcon color="primary" fontSize="medium"/>
-            </Icon>
-                    Our Mission and Core Values
-          </Typography>
-          <Typography variant="body1" color="text.secondary" component="div">
-            At NEXUS, we don&apos;t just provide telecommunications services, we build connections. Our mission is to become a solid bridge, bringing superior quality internet and telecommunications solutions, contributing to improving the quality of life of each individual and promoting the sustainable development of the community.
-
-            <br/><br/>
-
-            To fulfill this noble mission, NEXUS always adheres to the following core values:
-
-            <br/><br/>
-
-            <Typography variant="subtitle1" component="h3" fontWeight="bold">
-              * We are committed to providing customers with services and products that meet the <strong>highest quality standards</strong>.
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              From stable internet speeds, reliable transmission lines, to clear call quality, NEXUS always strives to excel in meeting and exceeding customer expectations.
-            </Typography>
-
-
-            <br/>
-            <Typography variant="subtitle1" component="h3" fontWeight="bold">
-              * <strong>Continuous innovation:</strong>
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              In a constantly changing technology world, <strong>innovation is the key</strong> for NEXUS to always lead. We constantly innovate, research and apply the most advanced technologies to bring pioneering, effective and suitable telecommunications solutions to the increasingly diverse needs of the market.
-            </Typography>
-
-
-            <br/>
-            <Typography variant="subtitle1" component="h3" fontWeight="bold">
-              * <strong>Dedication from the heart:</strong>
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              Customers are always the <strong>center</strong> of all NEXUS&apos;s activities. We build sustainable relationships with customers based on dedication, thoughtfulness and listening. NEXUS&apos;s professional and enthusiastic staff is always ready to support and solve all customer problems quickly and effectively.
-            </Typography>
-
-
-            <br/>
-            <Typography variant="subtitle1" component="h3" fontWeight="bold">
-              * <strong>Prestige and reliability:</strong>
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              NEXUS understands that <strong>prestige is a priceless asset</strong>. We build <strong>trust</strong> and <strong>credibility</strong> with customers, partners and the community through transparency, responsibility and commitment to do what we promise. NEXUS aims to become a <strong>reliable</strong> partner, a <strong>solid</strong> companion on each customer&apos;s development path.
-            </Typography>
-          </Typography>
-        </Box>
-        <Divider sx={{ mb: 4 }} />
-
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
-            <Icon
-              sx={{
-                mr: 1
-              }}
-            >
-              <PeopleRoundedIcon color="primary" fontSize="medium"/>
-            </Icon>
-                        Our Team
-          </Typography>
-          <Box sx={{ 
-            mt: 2, 
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '32px',
-            justifyContent: 'center'
-          }}>
-            {teamData.map((member) => (
-              <Box 
-                key={member.id}
+        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+          <Box sx={{ mb: 4 }}>
+            <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+              <Icon
                 sx={{
-                  width: { xs: '100%', sm: '45%', md: '30%' },
-                  textAlign: 'center',
-                  mb: 4
+                  mr: 1
                 }}
               >
-                <Avatar
-                  alt={member.name}
-                  src={member.avatarUrl}
-                  sx={{ width: 120, height: 120, mb: 1, mx: 'auto' }}
-                />
-                <Typography variant="h6" component="h3" gutterBottom>
-                  {member.name}
-                </Typography>
-                <Typography variant="subtitle2" color="text.secondary" gutterBottom>
-                  {member.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {member.description}
-                </Typography>
-              </Box>
-            ))}
+                <LightbulbRoundedIcon color="primary" fontSize="medium"/>
+              </Icon>
+                      Our Mission and Core Values
+            </Typography>
+            <Typography variant="body1" color="text.secondary" component="div">
+              At NEXUS, we don&apos;t just provide telecommunications services, we build connections. Our mission is to become a solid bridge, bringing superior quality internet and telecommunications solutions, contributing to improving the quality of life of each individual and promoting the sustainable development of the community.
+
+              <br/><br/>
+
+              To fulfill this noble mission, NEXUS always adheres to the following core values:
+
+              <br/><br/>
+
+              <Typography variant="subtitle1" component="h3" fontWeight="bold">
+                * We are committed to providing customers with services and products that meet the <strong>highest quality standards</strong>.
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                From stable internet speeds, reliable transmission lines, to clear call quality, NEXUS always strives to excel in meeting and exceeding customer expectations.
+              </Typography>
+
+
+              <br/>
+              <Typography variant="subtitle1" component="h3" fontWeight="bold">
+                * <strong>Continuous innovation:</strong>
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                In a constantly changing technology world, <strong>innovation is the key</strong> for NEXUS to always lead. We constantly innovate, research and apply the most advanced technologies to bring pioneering, effective and suitable telecommunications solutions to the increasingly diverse needs of the market.
+              </Typography>
+
+
+              <br/>
+              <Typography variant="subtitle1" component="h3" fontWeight="bold">
+                * <strong>Dedication from the heart:</strong>
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                Customers are always the <strong>center</strong> of all NEXUS&apos;s activities. We build sustainable relationships with customers based on dedication, thoughtfulness and listening. NEXUS&apos;s professional and enthusiastic staff is always ready to support and solve all customer problems quickly and effectively.
+              </Typography>
+
+
+              <br/>
+              <Typography variant="subtitle1" component="h3" fontWeight="bold">
+                * <strong>Prestige and reliability:</strong>
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                NEXUS understands that <strong>prestige is a priceless asset</strong>. We build <strong>trust</strong> and <strong>credibility</strong> with customers, partners and the community through transparency, responsibility and commitment to do what we promise. NEXUS aims to become a <strong>reliable</strong> partner, a <strong>solid</strong> companion on each customer&apos;s development path.
+              </Typography>
+            </Typography>
           </Box>
-        </Box>
+        </ScrollAnimation>
         <Divider sx={{ mb: 4 }} />
 
+        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+          <Box sx={{ mb: 4 }}>
+            <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+              <Icon
+                sx={{
+                  mr: 1
+                }}
+              >
+                <PeopleRoundedIcon color="primary" fontSize="medium"/>
+              </Icon>
+                      Our Team
+            </Typography>
+            <Box sx={{
+              mt: 2,
+              display: 'flex',
+              flexWrap: 'wrap',
+              gap: '32px',
+              justifyContent: 'center'
+            }}>
+              {teamData.map((member) => (
+                <Box
+                  key={member.id}
+                  sx={{
+                    width: { xs: '100%', sm: '45%', md: '30%' },
+                    textAlign: 'center',
+                    mb: 4
+                  }}
+                >
+                  <Avatar
+                    alt={member.name}
+                    src={member.avatarUrl}
+                    sx={{ width: 120, height: 120, mb: 1, mx: 'auto' }}
+                  />
+                  <Typography variant="h6" component="h3" gutterBottom>
+                    {member.name}
+                  </Typography>
+                  <Typography variant="subtitle2" color="text.secondary" gutterBottom>
+                    {member.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {member.description}
+                  </Typography>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </ScrollAnimation>
+        <Divider sx={{ mb: 4 }} />
 
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
-            {/* <Icon><CheckCircleRoundedIcon sx={{ mr: 1 }} color="primary" /> </Icon> */}
-                    Why Choose NEXUS?
-          </Typography>
-          <List>
-            {whyChooseUsData.map((reasonItem) => (
-              <ListItem key={reasonItem.id} disableGutters>
-                <ListItemIcon>
-                  {reasonItem.icon}
-                </ListItemIcon>
-                <ListItemText
-                  primary={reasonItem.reason}
-                  secondary={
-                    <Typography variant="body2" color="text.secondary">
-                      {reasonItem.reason}
-                    </Typography>
-                  }
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+          <Box sx={{ mb: 4 }}>
+            <Typography variant="h4" component="h2" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+              {/* <Icon><CheckCircleRoundedIcon sx={{ mr: 1 }} color="primary" /> </Icon> */}
+                      Why Choose NEXUS?
+            </Typography>
+            <List>
+              {whyChooseUsData.map((reasonItem) => (
+                <ListItem key={reasonItem.id} disableGutters>
+                  <ListItemIcon>
+                    {reasonItem.icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={reasonItem.reason}
+                    secondary={
+                      <Typography variant="body2" color="text.secondary">
+                        {reasonItem.reason}
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </ScrollAnimation>
 
-        <Box sx={{ textAlign: 'center', mt: 5 }}>
-          <Typography variant="h5" component="p" gutterBottom>
-                    Ready to experience superior connectivity from NEXUS?
-          </Typography>
-          <Button variant="contained" color="primary" size="large">
-                    Explore Services Now
-          </Button>
-        </Box>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+          <Box sx={{ textAlign: 'center', mt: 5 }}>
+            <Typography variant="h5" component="p" gutterBottom>
+                      Ready to experience superior connectivity from NEXUS?
+            </Typography>
+            <Button variant="contained" color="primary" size="large">
+                      Explore Services Now
+            </Button>
+          </Box>
+        </ScrollAnimation>
 
 
-        <Box mt={6} textAlign="center">
-          <Typography variant="body2" color="text.secondary">
-                    Want to learn more about NEXUS? <Link href="/contact">Contact us</Link>
-          </Typography>
-        </Box>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+          <Box mt={6} textAlign="center">
+            <Typography variant="body2" color="text.secondary">
+                      Want to learn more about NEXUS? <Link href="/contact">Contact us</Link>
+            </Typography>
+          </Box>
+        </ScrollAnimation>
       </Container>
     </Box>
   )
