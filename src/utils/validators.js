f69d9fd604eceacaeea83a8659a58da71c44e9ea
Quyendev <146425082+Grozay@users.noreply.github.com>
2025-03-04 12:25:46 +0700

    finsh add api inside page employee manage

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -5,19 +5,24 @@ export const ACCOUNT_ID_RULE = /^[a-zA-Z0-9]+$/
 export const ACCOUNT_ID_RULE_MESSAGE = 'Account ID is invalid. (only accept letters and numbers)'
 export const OTP_RULE = /^\d{6}$/
 export const OTP_RULE_MESSAGE = 'OTP is invalid. (only accept 6 numbers)'
+// export const PASSWORD_RULE = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d\W]{8,256}$/
 export const PASSWORD_RULE = /^\d{6}$/
-export const PASSWORD_RULE_MESSAGE = 'Mật khẩu phải chính xác 6 ký tự số.'
+export const PASSWORD_RULE_MESSAGE = 'Password must include at least 1 letter, a number, and at least 8 characters.'
 export const PASSWORD_CONFIRMATION_MESSAGE = 'Password Confirmation does not match!'
 export const SALARY_RULE_MESSAGE = 'Salary must be greater than 0'
 export const GENDER_RULE_MESSAGE = 'Gender is required.'
 export const DOB_RULE_MESSAGE = 'Date of Birth is invalid.'
 export const DOB_RULE = (value) => {
+  // Kiểm tra định dạng ngày tháng năm YYYY-MM-DD
   const regex = /^\d{4}-\d{2}-\d{2}$/
   if (!regex.test(value)) return false
-
+  // Chuyển đổi giá trị nhập vào thành đối tượng Date
   const inputDate = new Date(value)
+  // Lấy ngày hiện tại
   const currentDate = new Date()
-
+  // Đặt thời gian về 00:00:00 để chỉ so sánh ngày
+  currentDate.setHours(0, 0, 0, 0)
+  // Kiểm tra ngày nhập vào không lớn hơn ngày hiện tại
   return inputDate <= currentDate
 }
 
