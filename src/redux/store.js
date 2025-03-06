c44f14afb4b369ff2a6fdc0963048c9ca042805a
Le Anh Kiet <leakiet@users.noreply.github.com>
2025-03-06 08:27:54 -0600

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ const rootPersistConfig = {
   storage: storage, //biến storage ở trên - lưu vào localstorage
   whitelist: ['user', 'account', 'plan'], //định nghĩa các slice dữ liệu được phép duy trì qua mỗi lần f5 trình duyệt
   blacklist: [] //định nghĩa các slice dữ liệu không được phép duy trì qua mỗi lần f5 trình duyệt
-
+}
 
 //combine các reducer trong dự án của chúng ta ở đây
 const rootReducer = combineReducers({
